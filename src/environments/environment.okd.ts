3b9a02d848c66292a1f3ecd4efa0f0b42912270f
miguta kevin <migutak@gmail.com>
2020-03-26 16:59:22 +0300

    okd env

---
+++ src/environments/environment.okd.ts
@@ -0,0 +1,31 @@
+export const environment = {
+  production: true,
+  adlogin: false,
+  sendsms: true,
+  api: 'http://ecollectapis-ecollectapis.159.122.130.146.nip.io',
+  nodeapi: 'http://nodeapi-nodeapi.159.122.130.146.nip.io/nodeapi',
+  letters_path: '/home/ecollectadmin/demand_letters/',
+  letters_api: 'http://docx-docx.159.122.130.146.nip.io/docx/',
+  uploadurl: 'http://filesapi-filesapi.159.122.130.146.nip.io/filesapi',
+  demanddownload: 'http://demanddownload-demanddownload.159.122.130.146.nip.io/demanddownload',
+  valor: 'http://valor-valor.159.122.130.146.nip.io/valor',
+  applink: '',
+  xlsuploadapi: 'http://xlsupload-xlsupload.159.122.130.146.nip.io/xlsupload/',
+  xlstemplate: '/root/templates/upload_notes.xlsx',
+  emailapi: 'http://demandemail-demandemail.159.122.130.146.nip.io/demandemail/email',
+  demandsmsapi: "http://sms-sms.159.122.130.146.nip.io/sms",
+  auth: 'http://adlogin-adlogin.159.122.130.146.nip.io/adlogin',
+  filesapi: 'http://filesapi-filesapi.159.122.130.146.nip.io/filesapi',
+  accplanlink: 'http://accplan-accplan.159.122.130.146.nip.io',
+  kibanarpt: 'http://kibana-oss-kibana.159.122.130.146.nip.io',
+  manuals_path: '/app/manuals/',
+  birt: 'http://birt-birt.159.122.130.146.nip.io/birt',
+  kibana: 'http://kibana-oss-kibana.159.122.130.146.nip.io/app/kibana#/dashboard?_g=0',
+  portfoliodash: 'https://52.117.54.217:4403/goto/bf80fe14782362d8f9307501e75bf055?embed=true',
+  portal: 'http://portal-portal.159.122.130.146.nip.io/portal',
+  accplanreport: 'http://52.117.54.217:5601/app/kibana#/discover/346b8c30-0b89-11ea-8410-c5d8870411ab?_g=()',
+  ptpreport: 'http://52.117.54.217:5601/app/kibana#/discover/6f42b0b0-0b8c-11ea-8410-c5d8870411ab?_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Cto%3Anow))'
+  ,homedash: 'http://172.16.19.151:5601/goto/f893ccd125308885e80090233919f80e?embed=true',
+  workflow: 'http://52.117.54.217:8089/sysworkflow/en/neoclassic/login/login'
+};
+
