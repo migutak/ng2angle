fe7c0447c72c6082d0e9cfc8e16c74ebcdc11aeb
Kevin Miguta <migutak@gmail.com>
2020-05-19 20:50:30 +0300

    relegate start

--- src/app/routes/teamleader/relegate/relegate.component.ts
+++ src/app/routes/teamleader/relegate/relegate.component.ts
@@ -1,27 +1,153 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as _ from 'lodash';
-declare var $: any;
+import { Component, OnInit } from '@angular/core';
+import {AllModules} from '@ag-grid-enterprise/all-modules';
 import { environment } from '../../../../environments/environment';
 
-
 @Component({
-    selector: 'app-relegate',
-    templateUrl: './relegate.component.html',
-    styleUrls: ['./relegate.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-relegate',
+  templateUrl: './relegate.component.html',
+  styleUrls: ['./relegate.component.scss']
 })
+
 export class RelegateComponent implements OnInit {
+  public gridApi;
+  public gridColumnApi;
 
+  public columnDefs;
+  public defaultColDef;
+  public rowModelType;
+  public cacheBlockSize;
+  public maxBlocksInCache;
+  public rowData: [];
 
+  currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  username: string;
+  searchText: string;
+  model: any = {};
 
-    constructor(
+  modules = AllModules;
 
-    ) {
+  constructor() {
+    this.columnDefs = [
+      {
+        field: 'ACCNUMBER',
+        cellRenderer: function (params) {
+          if (params.value !== undefined) {
+            return '<a  href="#" target="_blank">' + params.value + '</a>';
+          } else {
+            return '<img src="assets/img/user/loading.gif">';
+          }
+        },
+        filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true
+      },
+      { field: 'CUSTNAME', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'CUSTNUMBER', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'DAYSINARR', filter: 'agNumberColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'PRODUCTCODE', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'AROCODE', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'RROCODE', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      {
+        field: 'OUSTBALANCE',
+        cellRenderer: function (params) {
+          if (params.value !== undefined) {
+            return (Math.floor(params.value * 100) / 100).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+          } else {
+            return ''
+          }
+        },
+        filter: 'agNumberColumnFilter', filterParams: { newRowsAction: 'keep' }, aggFunc: 'sum', resizable: true
+      },
+      {
+        field: 'SCHEDULEAMOUNT',
+        cellRenderer: function (params) {
+          if (params.value !== undefined) {
+            return (Math.floor(params.value * 100) / 100).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+          } else {
+            return ''
+          }
+        },
+        filter: 'agNumberColumnFilter', filterParams: { newRowsAction: 'keep' }, aggFunc: 'sum', resizable: true
+      },
+      { field: 'SCHEDULEDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'ORIGDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'SCHEDULEDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'INSURANCE', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'STATUS', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'STAGEDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'CANCELLATIONDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'PAYMENTDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'REINSTATEMENTDATE', filter: 'agDateColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true },
+      { field: 'CASENUMBER', filter: 'agTextColumnFilter', filterParams: { newRowsAction: 'keep' }, resizable: true }
+    ];
+    this.defaultColDef = {
+      width: 120,
+      resizable: true,
+      sortable: true,
+      floatingFilter: true
+    };
+    this.rowModelType = "serverSide";
+    this.cacheBlockSize = 50;
+    this.maxBlocksInCache = 0;
+  }
 
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    const datasource = {
+      getRows(params) {
+
+        fetch(environment.api + '/api/tbl_ipf/gridviewall', {
+          method: 'post',
+          body: JSON.stringify(params.request),
+          headers: { "Content-Type": "application/json; charset=utf-8" }
+        })
+          .then(httpResponse => httpResponse.json())
+          .then(response => {
+            params.successCallback(response.rows, response.lastRow);
+          })
+          .catch(error => {
+            console.error(error);
+            params.failCallback();
+          })
+      }
+    };
+
+    params.api.setServerSideDatasource(datasource);
+  }
+
+  ServerSideDatasource(server) {
+    return {
+      getRows(params) {
+        setTimeout(function () {
+          var response = server.getResponse(params.request);
+          if (response.success) {
+            params.successCallback(response.rows, response.lastRow);
+          } else {
+            params.failCallback();
+          }
+        }, 500);
+      }
+    };
+  }
+
+  currencyFormatter(params) {
+    if (params.value !== undefined) {
+      return (Math.floor(params.value * 100) / 100).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    } else {
+      return ''
     }
+  }
 
-    ngOnInit() { }
+  onRowDoubleClicked(event: any) {
+    this.model = event.node.data;
+    // tslint:disable-next-line:max-line-length
+    window.open(environment.applink + '/activitylog?accnumber=' + this.model.ACCNUMBER + '&custnumber=' + this.model.CUSTNUMBER + '&username=' + this.username + '&sys=assetf', '_blank');
+  }
 
 
+  public ngOnInit(): void {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.username = currentUser.USERNAME;
+  }
 
 }
--- src/app/routes/teamleader/teamleader.module.ts
+++ src/app/routes/teamleader/teamleader.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TreeModule } from 'angular-tree-component';
-
+import { AgGridModule } from '@ag-grid-community/angular';
 import { SharedModule } from '../../shared/shared.module';
 import { RelegateComponent } from './relegate/relegate.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'relegate' },
-    { path: 'relegate', component: RelegateComponent }
+    { path: '', redirectTo: 'accounts' },
+    { path: 'accounts', component: RelegateComponent }
 ];
 
 @NgModule({
     imports: [
         SharedModule,
-        TreeModule.forRoot(),
+        AgGridModule.withComponents([]),
         RouterModule.forChild(routes)
     ],
     declarations: [
